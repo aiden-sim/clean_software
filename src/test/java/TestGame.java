ce35e34a45e03922cf28f4f6ec77cfb2edb3d8e0
Unknown <simjunbo@nate.com>
2018-02-08 00:04:37 +0900

    테스트코드 정리

--- src/test/java/TestGame.java
+++ src/test/java/TestGame.java
@@ -11,23 +11,10 @@ public class TestGame extends TestCase {
         g = new Game();
     }
 
-    public void testOneThrow() {
-        g.add(5);
-        // 테스트 케이스는 목적에 부합하지 않으면 삭제가능
-        // assertEquals(5, g.score()); 처음 프레임의 첫 투구만으로는 score를 계산할 수 없다.
-        assertEquals(1, g.getCurrentFrame());
-    }
-
-    public void testOneThrowAndStrike() {
-        g.add(10);
-        assertEquals(10, g.score());
-    }
-
     public void testTwoThrowsNoMark() {
         g.add(5);
         g.add(4);
         assertEquals(9, g.score());
-        assertEquals(2, g.getCurrentFrame());
     }
 
     public void testFourThrowsNoMark() {
@@ -38,7 +25,6 @@ public class TestGame extends TestCase {
         assertEquals(18, g.score());
         assertEquals(9, g.scoreForFrame(1));
         assertEquals(18, g.scoreForFrame(2));
-        assertEquals(3, g.getCurrentFrame());
     }
 
     public void testSimpleSpare() {
@@ -46,7 +32,6 @@ public class TestGame extends TestCase {
         g.add(7);
         g.add(3);
         assertEquals(13, g.scoreForFrame(1));
-        assertEquals(2, g.getCurrentFrame());
     }
 
     public void testSimpleFrameAfterSpare() {
@@ -57,16 +42,14 @@ public class TestGame extends TestCase {
         assertEquals(13, g.scoreForFrame(1));
         assertEquals(18, g.scoreForFrame(2));
         assertEquals(18, g.score());
-        assertEquals(3, g.getCurrentFrame());
     }
 
-    public void testSimpleStrile() {
+    public void testSimpleStrilke() {
         g.add(10);
         g.add(3);
         g.add(6);
         assertEquals(19, g.scoreForFrame(1));
         assertEquals(28, g.score());
-        assertEquals(3, g.getCurrentFrame());
     }
 
     public void testPerfectGame() {
@@ -74,7 +57,6 @@ public class TestGame extends TestCase {
             g.add(10);
         }
         assertEquals(300, g.score());
-        assertEquals(11, g.getCurrentFrame()); // 좀 찝찝함
     }
 
     public void testEndOfArray() {
