8184409b648aac69bb749c5ce2fe14447ddff83f
Unknown <simjunbo@nate.com>
2018-02-07 20:04:50 +0900

    Frame 추가 (62-63P)

---
+++ src/main/java/Frame.java
@@ -0,0 +1,8 @@
+/**
+ * Created by simjunbo on 2018-02-07.
+ */
+public class Frame {
+    public int getScore() {
+        return 0;
+    }
+}
---
+++ src/test/java/TestFrame.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+public class TestFrame extends TestCase {
+    public TestFrame(String name) {
+        super(name);
+    }
+
+    public void testScoreNoThrows() {
+        Frame f = new Frame();
+        f.add(5);
+        assertEquals(0, f.getScore());
+    }
+}
