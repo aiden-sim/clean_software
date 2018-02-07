7accc0b637b25f9d99e3bd5235ecfa0bd9060d51
Unknown <simjunbo@nate.com>
2018-02-08 00:00:45 +0900

    itsCurrentFrame 기준 변경

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,11 +1,11 @@
 
 public class Game {
     private int itsCurrentFrame = 1; // 현재 프레임 계산
-    private boolean firstThrowFrame = true; // 프레임에서 첫번째 투구 여부 확인
+    private boolean firstThrowInFrame = true; // 프레임에서 첫번째 투구 여부 확인
     private Scorer itsScorer = new Scorer();
 
     public int score() {
-        return scoreForFrame(getCurrentFrame() - 1);
+        return scoreForFrame(itsCurrentFrame);
     }
 
     public void add(int pins) {
@@ -14,12 +14,10 @@ public class Game {
     }
 
     private void adjustCurrentFrame(int pins) {
-        if (firstThrowFrame == true) {
-            if(adjustFrameForStrike(pins) == false)
-                firstThrowFrame = false;
-        } else {
-            firstThrowFrame = true;
+        if (lastBallInFrame(pins)) {
             advanceFrame();
+        } else {
+            firstThrowInFrame = false;
         }
     }
 
@@ -32,14 +30,14 @@ public class Game {
     }
 
     private void advanceFrame() {
-        itsCurrentFrame = Math.min(11, itsCurrentFrame + 1); // itsCurrentFrame은 종료의미기 때문에 10frame 인경우 11로 되어 있다.
+        itsCurrentFrame = Math.min(10, itsCurrentFrame + 1); // itsCurrentFrame은 종료의미기 때문에 10frame 인경우 11로 되어 있다.
     }
 
-    private boolean adjustFrameForStrike(int pins) {
-        if (pins == 10) {
-            advanceFrame();
-            return true;
-        }
-        return false;
+    private boolean strike(int pins) {
+        return (firstThrowInFrame && pins == 10);
+    }
+
+    private boolean lastBallInFrame(int pins) {
+        return strike(pins) || !firstThrowInFrame;
     }
 }
