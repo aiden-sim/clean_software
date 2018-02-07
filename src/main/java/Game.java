95b76360ba1569f153cad98d6dba3abf18999517
Unknown <simjunbo@nate.com>
2018-02-07 20:20:57 +0900

    Game 추가(67P)

---
+++ src/main/java/Game.java
@@ -0,0 +1,12 @@
+
+public class Game {
+    private int itsScore = 0;
+
+    public int score() {
+        return itsScore;
+    }
+
+    public void add(int pins) {
+        itsScore += pins;
+    }
+}
---
+++ src/test/java/TestGame.java
@@ -0,0 +1,13 @@
+import junit.framework.TestCase;
+
+public class TestGame extends TestCase {
+    public TestGame(String name) {
+        super(name);
+    }
+
+    public void testOneThrow() {
+        Game g = new Game();
+        g.add(5);
+        assertEquals(5, g.score());
+    }
+}
