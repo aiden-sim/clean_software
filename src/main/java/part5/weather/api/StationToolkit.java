b7ba63f9f228388cf6993a02dadf0a532babf4c6
simjunbo <simjunbo@tmon.co.kr>
2018-04-04 23:17:54 +0900

    commit

--- src/main/java/part5/weather/api/StationToolkit.java
+++ src/main/java/part5/weather/api/StationToolkit.java
@@ -19,6 +19,8 @@ public interface StationToolkit
   TemperatureSensorImp makeTemperature();
   BarometricPressureSensorImp makeBarometricPressure();
 
-  PersistentImp getPersistentImp();
   AlarmClockImp getAlarmClock();
+
+  PersistentImp getPersistentImp();
+
 }
